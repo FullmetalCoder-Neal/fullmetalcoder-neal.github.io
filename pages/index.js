8b4a378064202e0cfbd419665f26f1f1249f8933
Neal <neal1991@qq.com>
2025-04-28 20:17:44 +0800

    feat: update

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hello World]</p>
+        <p>Make Programming Great Again!</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -19,8 +19,6 @@ export default function Home({ allPostsData }) {
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              {id}
               <br />
               {date}
             </li>
